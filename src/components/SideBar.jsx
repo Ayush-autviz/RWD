30ba37f05edebdc71e7d8c2186c26493f27303c4
ishu parmar <ishuparmar529>
2024-07-11 15:34:54 +0530

    search

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,28 +7,40 @@ const SideBar = () => {
   const navigate = useNavigate();
   const { selectedTab, setSelectedTab } = useTabContext();
   return (
-    <div className="h-[100vh] w-[82px] flex flex-col justify-between shadow-custom ">
-      <div className="cursor-pointer">
-        <img src="Frame 1.svg" />
-      </div>
-      <div className=" flex flex-col gap-[61px] justify-between items-center">
-      <Link onClick={()=>{setSelectedTab('home')}} to='/'>
-      <img  className='cursor-pointer image-color' src="ri_home-3-line.svg" />
-      </Link>
-      <Link onClick={()=>{setSelectedTab('search')}} to='/search'>
-      <img  className='cursor-pointer image-color' src="mdi_search.svg" />
-      </Link>
-      <Link onClick={()=>{setSelectedTab('user')}} to='/profile'>
-      <img  className='cursor-pointer image-color' src="lucide_user.svg" />
-      </Link>
-      </div>
-      <div onClick={()=>{
-        localStorage.removeItem('isAuthenticated');
-        navigate('/login')
-      }}>
-        <img className='cursor-pointer image-brown' src="humbleicons_logout.svg" />
-      </div>
+    <div className="h-[100vh] w-[82px] flex flex-col justify-between shadow-custom">
+    <div className="cursor-pointer">
+      <img src="Frame 1.svg" alt="Logo" />
     </div>
+    <div className="flex flex-col gap-[61px] justify-between items-center">
+      <Link onClick={() => setSelectedTab('home')} to='/'>
+        <img 
+          className={`cursor-pointer image-color ${selectedTab === 'home' ? 'green' : ''}`} 
+          src="ri_home-3-line.svg" 
+          alt="Home" 
+        />
+      </Link>
+      <Link onClick={() => setSelectedTab('search')} to='/search'>
+        <img 
+          className={`cursor-pointer image-color ${selectedTab === 'search' ? 'green' : ''}`} 
+          src="mdi_search.svg" 
+          alt="Search" 
+        />
+      </Link>
+      <Link onClick={() => setSelectedTab('user')} to='/profile'>
+        <img 
+          className={`cursor-pointer image-color ${selectedTab === 'user' ? 'green' : ''}`} 
+          src="lucide_user.svg" 
+          alt="User" 
+        />
+      </Link>
+    </div>
+    <div onClick={() => {
+      localStorage.removeItem('isAuthenticated');
+      navigate('/login');
+    }}>
+      <img className='cursor-pointer image-brown' src="humbleicons_logout.svg" alt="Logout" />
+    </div>
+  </div>
   );
 };
 
